55670a42e8ec85a381e2b932a667bd3b516052ae
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-10-16 22:47:49 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "../styles/Hero.css";
 import mainHero from "../images/mainHero.png";
+import mainHeroMobile from "../images/mainHeroMobile.png";
 
 function Hero() {
   return (
     <section className="hero__section">
       <div className="hero__image-container">
         <img className="responsive hero__image" src={mainHero} alt="hero" />
+        <img
+          className="responsive hero__image-mobile"
+          src={mainHeroMobile}
+          alt="hero"
+        />
         <div className="hero__heading-container">
           <h2>We don't water down.</h2>
           <h2>We water up.</h2>
